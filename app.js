8d0ffad5256ecf22ebfc56f34ea68bb02194d5f4
Dave Abhishek <abhishekjdave0309@gmail.com>
2024-07-17 22:35:45 +0530

    create register,login,logout

---
+++ app.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+const userModel = require("./models/user");
+const postModel = require("./models/post");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+app.set("view engine", "ejs");  
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/register", async (req, res) => {
+  let { username, name, age, email, password } = req.body;
+  // check that this user already exist or not
+  let user = await userModel.findOne({ email}); // sort version of userModel.findOne({email: email});
+  if (user) {
+    return res.status(500).send("user already registered");
+  }
+
+  // here i hash my password
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      let user =await userModel.create({
+        username,
+        name,
+        age,
+        email,
+        password:hash
+      });
+
+      let token = jwt.sign({email: email, userid:user._id}, "shhhhh");
+      res.cookie("token", token);
+      res.send("registered");
+    });
+  });
+});
+
+app.get("/login", async (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", async (req, res) => {
+  let {email, password } = req.body;
+  let user = await userModel.findOne({ email }); // sort version of userModel.findOne({email: email});
+  if (!user) {
+    return res.status(500).send("something went wrong");
+  }
+
+  bcrypt.compare(password, user.password, function(err, result){
+    if(result){
+      let token = jwt.sign({email: email, userid:user._id}, "shhhhh");
+      res.cookie("token", token);
+      res.status(200).send("you can login");
+    } 
+     else res.redirect("/login");
+  });
+
+});
+
+app.get("/profile", isLoggdIn,async (req, res) => {
+  res.send(req.user);
+  res.send("your profile here");
+});
+
+app.get("/logout", async (req, res) => {
+  res.cookie("token", "");
+  res.redirect("/login");
+});
+
+function isLoggdIn(req, res, next){
+  if(req.cookies.token == "") res.send("you must be logged in");
+  else{
+    let data = jwt.verify(req.cookies.token, "shhhhh");
+    req.user = data;
+    next();
+  }
+}
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Example app listening on port 3000`);
+});
